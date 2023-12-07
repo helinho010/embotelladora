a0aa3f440ff20a2efd24e918bcc6000c10dc170b
hacker <mm.helio009@gmail.com>
2023-12-07 01:14:47 -0400

    prueba de commit

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,4 +2,5 @@
     How To Install Vue 3 in Laravel 10 with Vite - TechvBlogs
     <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda neque earum, totam atque in, expedita eveniet ratione fuga a velit cupiditate sint nihil itaque repellendus molestias nisi sunt libero amet.</h1>
     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum reiciendis obcaecati quam repellat magni eum dignissimos eos quaerat nostrum natus non sequi hic, perspiciatis sit inventore qui voluptatibus minus! Reprehenderit.</h2>
+    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nemo aut alias fuga necessitatibus at error, tenetur molestias perferendis, enim, corporis minus incidunt ea odit asperiores nostrum! Consequatur, magnam rem?</h3>
 </template>

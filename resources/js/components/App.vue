6f2a5f98503e5a70a1e4f614e98571fa31b1cb97
hacker <mm.helio009@gmail.com>
2023-12-07 00:08:25 -0400

    Proyecto inicial embotelladora

---
+++ resources/js/components/App.vue
@@ -0,0 +1,4 @@
+<template>
+    How To Install Vue 3 in Laravel 10 with Vite - TechvBlogs
+    <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda neque earum, totam atque in, expedita eveniet ratione fuga a velit cupiditate sint nihil itaque repellendus molestias nisi sunt libero amet.</h1>
+</template>

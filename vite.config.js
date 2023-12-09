9850d1c178b3849fcf2640fad98158c03ca600d9
hacker <mm.helio009@gmail.com>
2023-12-09 00:09:19 -0400

    Se instalo el login ui de laravel - bootstrap - blade

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/bootstrap/css/bootstrap.css', 'resources/bootstrap/js/bootstrap.min.js', 'resources/bootstrap-icons/font/bootstrap-icons.min.css'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
